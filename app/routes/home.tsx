9951f6322e058f32ef96017331e8debf2b9e925f
Richard Giddings <rmpgiddings@gmail.com>
2026-03-20 15:13:43 +0000

    Display start time of event with the date

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,7 +16,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
 	return [
 		{ title: "GoodGym Events" },
 		{ name: "description", content: "Events in a GoodGym area" },
@@ -24,36 +24,36 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function clientLoader({
-  	params,
+	params,
 }: Route.ClientLoaderArgs) {
 	const api_url = import.meta.env.VITE_APP_URL;
 	const center_longitude = Number(import.meta.env.VITE_MAP_CENTER_LONGITUDE);
 	const center_latitude = Number(import.meta.env.VITE_MAP_CENTER_LATITUDE);
 
-	const res = await fetch(api_url+"/events/")
+	const res = await fetch(api_url + "/events/")
 	const event_data = await res.json();
 
 	const events = event_data.events;
 	const locations = event_data.locations;
 
-	return {events, locations, center_longitude, center_latitude};
+	return { events, locations, center_longitude, center_latitude };
 }
 
 function getFormattedDate(input_date: string) {
 
-    const date = new Date(input_date);
+	const date = new Date(input_date);
 
-	const timeFormat: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+	const timeFormat: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', hour: '2-digit', minute: '2-digit', hour12: false, day: 'numeric' };
 	var formatted_date = date.toLocaleDateString("en-GB", timeFormat);
 
-    return formatted_date;
+	return formatted_date;
 }
 
 export default function Home({
-  	loaderData
+	loaderData
 }: Route.ComponentProps) {
 
-	const {events, locations, center_longitude, center_latitude} = loaderData;
+	const { events, locations, center_longitude, center_latitude } = loaderData;
 
 	// For constructing the url as data doesn't contain correct link
 	const base_url = "https://www.goodgym.org/v3/sessions/";
@@ -61,7 +61,7 @@ export default function Home({
 	const end = "?";
 
 	// Maps
-	const position = {lat: center_latitude, lng: center_longitude}
+	const position = { lat: center_latitude, lng: center_longitude }
 	const api_key = import.meta.env.VITE_MAPS_API_KEY;
 	const map_id = import.meta.env.VITE_MAP_ID
 	const [open, setOpen] = useState(Array(locations.length).fill(false));
@@ -80,7 +80,7 @@ export default function Home({
 
 	// Filtering 
 	const [filter, setFilter] = useState("");
-	const filteredLocations = 
+	const filteredLocations =
 		filter === ""
 			? locations
 			: locations.filter((location: any) => location.event_type.replace(' ', '-').toLowerCase() === filter)
@@ -97,32 +97,32 @@ export default function Home({
 				</Card.Header>
 				<Card.Body className="p-2">
 					<APIProvider apiKey={api_key}>
-						<div style={{height: "50vh"}}>
+						<div style={{ height: "50vh" }}>
 							<Map defaultZoom={11} defaultCenter={position} mapId={map_id} disableDefaultUI={true}>
 								{filteredLocations.map((location: any) => (
-								<AdvancedMarker 
-									key={location.name}
-									position={location.position}
-									title={location.name}
-									onClick={() => handleClick(location.number, true)}>
-									<Pin background={location.background} glyphColor="black" scale={1.3}></Pin>
-									{open[location.number] === true && (
-										<InfoWindow position={location.position} onCloseClick={() => handleClick(location.number, false)}>
-										<p className="line-break">{location.name}</p>
-										</InfoWindow>
-									)}
-								</AdvancedMarker>
+									<AdvancedMarker
+										key={location.name}
+										position={location.position}
+										title={location.name}
+										onClick={() => handleClick(location.number, true)}>
+										<Pin background={location.background} glyphColor="black" scale={1.3}></Pin>
+										{open[location.number] === true && (
+											<InfoWindow position={location.position} onCloseClick={() => handleClick(location.number, false)}>
+												<p className="line-break">{location.name}</p>
+											</InfoWindow>
+										)}
+									</AdvancedMarker>
 								))}
 							</Map>
 						</div>
-					</APIProvider>	
+					</APIProvider>
 					<Container className="pt-2 barlow-condensed-semibold" fluid>
 						<Row>
 							<Col lg className="p-2 mt-1 group-run" onClick={() => setFilter('group-run')}>Group Runs</Col>
 							<Col lg className="p-2 mt-1 community-mission" onClick={() => setFilter('community-mission')}>Community Mission</Col>
 							<Col lg className="p-2 mt-1 party" onClick={() => setFilter('party')}>Party/Eats</Col>
 							<Col lg className="p-2 mt-1 race" onClick={() => setFilter('race')}>Race</Col>
-							<Col lg className="p-2 mt-1 training-session" onClick={() => setFilter('training-session')}>Training Session</Col>							
+							<Col lg className="p-2 mt-1 training-session" onClick={() => setFilter('training-session')}>Training Session</Col>
 						</Row>
 					</Container>
 				</Card.Body>
@@ -131,28 +131,28 @@ export default function Home({
 					<ul>
 						<li>Zoom in and out and move the map.</li>
 						<li>Rollover or select a pin to see the event name(s).</li>
-						<li>Select an event type above to show only that type of event.</li> 
+						<li>Select an event type above to show only that type of event.</li>
 						<li>Click <Button className="p-0 links" variant="link" onClick={() => setFilter('')}>here</Button> to show all events again.</li>
 					</ul>
 					For more details on events see the list below.
 				</Card.Footer>
 			</Card>
 
-			<hr/>
+			<hr />
 
 			{filteredEvents.map((ev: any) => (
-			<Card key={ev.id} bg="light" text="dark" className="mt-4">
-				<Card.Header className={ev.data.programme.name.replace(' ', '-').toLowerCase()+"-card barlow-condensed-semibold"}>
-					{ev.data.programme.name}
-				</Card.Header>
-				<Card.Body>
-					<Card.Title className="mb-2">{ev.data.name}</Card.Title>
-					<Card.Subtitle className="mb-4 text-muted">{ev.data.disambiguatingDescription}</Card.Subtitle>
-					<MarkdownView markdown={ev.data["beta:formattedDescription"]}/>
-					<Card.Link href={base_url + ev.data.url.substring(ev.data.url.lastIndexOf('/') + 1).split(end)[0]} className="links" target="_blank">Event Link</Card.Link>
-				</Card.Body>
-				<Card.Footer>{getFormattedDate(ev.data.startDate)}</Card.Footer>
-			</Card>
+				<Card key={ev.id} bg="light" text="dark" className="mt-4">
+					<Card.Header className={ev.data.programme.name.replace(' ', '-').toLowerCase() + "-card barlow-condensed-semibold"}>
+						{ev.data.programme.name}
+					</Card.Header>
+					<Card.Body>
+						<Card.Title className="mb-2">{ev.data.name}</Card.Title>
+						<Card.Subtitle className="mb-4 text-muted">{ev.data.disambiguatingDescription}</Card.Subtitle>
+						<MarkdownView markdown={ev.data["beta:formattedDescription"]} />
+						<Card.Link href={base_url + ev.data.url.substring(ev.data.url.lastIndexOf('/') + 1).split(end)[0]} className="links" target="_blank">Event Link</Card.Link>
+					</Card.Body>
+					<Card.Footer>{getFormattedDate(ev.data.startDate)}</Card.Footer>
+				</Card>
 			))}
 			<p className="mt-4">
 				Created using <a href="https://www.openactive.io/" className="links">OpenActive</a> data from <a href="https://github.com/good-gym/opendata" className="links">GoodGym</a> under the <a href="https://creativecommons.org/licenses/by/4.0/" className="links">Creative Commons Attribution Licence</a>. The front-end code for this site can be found <a href="https://github.com/richardgiddings/goodgym-events-frontend" className="links">here</a> and the API can be found <a href="https://github.com/richardgiddings/goodgym-events-api" className="links">here</a>.
